f25523a644e1076893a301bf419809e21a39f3cc
Yash <jauhariyash23@gmail.com>
2023-07-06 18:04:56 +0530

    added react-router-dom

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import { Link } from 'react-router-dom';
+const Home=()=>{
+   return (<div>
+    <Link to="/starred">
+        Go to starred apge
+    </Link>
+   </div>);
+}
+
+export default Home;
