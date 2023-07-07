ffcd6a62b605e5cdee3246133a12c08776f37531
Yash <jauhariyash23@gmail.com>
2023-07-07 13:53:53 +0530

    search code refactored

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,31 +1,25 @@
 import { useState } from 'react';
 import { searchforShows } from '../api/tvmaze';
 import { searchforPeople } from '../api/tvmaze';
+import SearchForm from '../Components/SerachForm';
 const Home = () => {
-  const [searchStr, setSearchStr] = useState('');
+ 
   const [apiData, setApiData] = useState(null);
   const [apiDataError, setApiDataError] = useState(null);
-  const [searchOption,setSearchOption]=useState('shows')
-  console.log(searchOption);
   
-  const onSearchInputChange = ev => {
-    setSearchStr(ev.target.value);
-  };
+  
+  
+ 
 
-  const onRadioChange=ev=>{
-    setSearchOption(ev.target.value)
-  }
-
-  const onSearch = async ev => {
-    ev.preventDefault();
+  const onSearch = async ({q,searchOption}) => {
     try {
       if(searchOption==="shows"){
       setApiDataError(null);
-      const result = await searchforShows(searchStr);
+      const result = await searchforShows(q);
       setApiData(result);
       }
       else{
-        const result = await searchforPeople(searchStr);
+        const result = await searchforPeople(q);
         setApiData(result);
       }
     } catch (error) {
@@ -50,23 +44,10 @@ const Home = () => {
 
   return (
     <div>
-      Home
-      <form onSubmit={onSearch}>
-        <input type="text" value={searchStr} onChange={onSearchInputChange} />
-        
-        <label>
-            Shows
-            <input type="radio" name="search-option" value="shows" checked={searchOption==="shows"} onChange={onRadioChange}/>
-        </label>
-
-        <label>
-            Actors
-            <input type="radio" name="search-option" value="actors" checked={searchOption==="actors"} onChange={onRadioChange}/>
-        </label>
-        
-        <button type="submit">Search</button>
-      </form>
-      <div>{renderApiData()}</div>
+    <SearchForm onSearch={onSearch}/>
+      {/* Home
+       */}
+      <div>{renderApiData()}</div> 
     </div>
   );
 };
